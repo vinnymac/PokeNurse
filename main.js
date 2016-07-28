b25cbad1ed292a198c19860b562116a0e9484f1c
Dominic Sciascia <dominic@duhminick.com>
2016-07-28 03:17:03 -0400

    created the login page

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const {app, BrowserWindow} = require('electron')
 let win
 
 function createWindow () {
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 800, height: 350})
   win.loadURL(`file://${__dirname}/login.html`)
 
   win.on('closed', () => {
