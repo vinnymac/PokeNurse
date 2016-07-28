ac62add00d1f5c38add254e991ccf02a087c59b4
Dominic <dominic@duhminick.com>
2016-07-28 02:31:06 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const {app, BrowserWindow} = require('electron')
+
+let win
+
+function createWindow () {
+  win = new BrowserWindow({width: 800, height: 600})
+  win.loadURL(`file://${__dirname}/login.html`)
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
