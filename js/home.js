3642df50a4486d3d99fe546119dd71080b382be9
Dominic <dominic@duhminick.com>
2016-07-29 19:52:49 -0400

    rough list of pokemons added and backgrounds for teams added

--- js/home.js
+++ js/home.js
@@ -1,1 +1,42 @@
-alert('test')
+const ipc = require('electron').ipcRenderer
+
+const header = document.getElementById('header')
+const usernameH = document.getElementById('username-h')
+const refreshBtn = document.getElementById('refresh-btn')
+const pokemonList = document.getElementById('pokemon-list')
+
+var playerInfo = ipc.sendSync('get-player-info')
+if (playerInfo.success) {
+  switch (playerInfo.player_data['team']) {
+    case 1:
+      header.style.backgroundImage = 'url("./imgs/mystic.jpg")'
+      break
+    case 2:
+      header.style.backgroundImage = 'url("./imgs/valor.jpg")'
+      break
+    case 3:
+      header.style.backgroundImage = 'url("./imgs/instinct.jpg")'
+      break
+  }
+
+  usernameH.innerHTML = playerInfo.player_data['username']
+
+  refreshPokemonList()
+} else {
+  ipc.send('error-message', 'Failed in retrieving player info.  Please restart.')
+}
+
+refreshBtn.addEventListener('click', () => {
+  refreshPokemonList()
+})
+
+function refreshPokemonList () {
+  var pokemons = ipc.sendSync('get-players-pokemons')
+  if (pokemons.success) {
+    pokemonList.innerHTML = ''
+
+    pokemons.pokemon.forEach(poke => {
+      pokemonList.innerHTML += '<tr><td>' + poke['pokemon_id'] + '</td><td>' + 'pokemon name' + '</td><td>' + poke['cp'] + '</td></tr>'
+    })
+  }
+}
--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 const {app, BrowserWindow, ipcMain, dialog} = require('electron')
 const fs = require('fs')
 const path = require('path')
-// var pokemon = require('pokemon-go-node-api')
+const pogobuf = require('pogobuf')
 
 const accountPath = path.join(app.getPath('appData'), '/pokenurse/account.json')
 
 let win
+let client
 
-function createWindow () {
-  win = new BrowserWindow({width: 800, height: 375})
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 375, title: 'PokéNurse', icon: 'imgs/emojioneicon.png' })
   win.setMenu(null)
   win.loadURL(`file://${__dirname}/login.html`)
+
+  client = new pogobuf.Client()
 
   win.on('closed', () => {
     win = null
@@ -59,7 +62,7 @@ ipcMain.on('get-account-credentials', (event) => {
   // Maybe use readFile instead
   credentials = JSON.parse(fs.readFileSync(accountPath))
 
-  console.log('[+] Saved account retrieved')
+  console.log('[+] Retrieved saved account')
 
   event.returnValue = {
     success: true,
@@ -80,7 +83,7 @@ ipcMain.on('save-account-credentials', (event, method, username, password) => {
 
   fs.writeFile(accountPath, credentials, (err) => {
     if (err) console.log(err)
-    console.log('[+] Account credentials saved to ' + accountPath)
+    console.log('[+] Saved account credentials to ' + accountPath)
   })
 })
 
@@ -95,5 +98,58 @@ ipcMain.on('check-and-delete-credentials', (event) => {
 
 ipcMain.on('pokemon-login', (event, method, username, password) => {
   console.log('[+] Attempting to login')
+
+  var login
+  if (method === 'google') {
+    login = new pogobuf.GoogleLogin()
+  } else {
+    login = new pogobuf.PTCLogin()
+  }
+
+  login.login(username, password).then(token => {
+    client.setAuthInfo(method, token)
+    client.init()
+
+    win.loadURL(`file://${__dirname}/home.html`)
+    win.setSize(900, 600, true)
+  })
 })
 // END OF LOGIN
+
+// POKEMON
+ipcMain.on('get-player-info', (event) => {
+  console.log('[+] Retrieving player info')
+  client.getPlayer().then(response => {
+    if (!response['success']) {
+      event.returnValue = {
+        success: false
+      }
+      return
+    }
+
+    event.returnValue = {
+      success: 'true',
+      player_data: response['player_data']
+    }
+  })
+})
+
+ipcMain.on('get-players-pokemons', (event) => {
+  console.log('[+] Retrieving player\'s Pokemons')
+  client.getInventory(0).then(inventory => {
+    if (!inventory['success']) {
+      event.returnValue = {
+        success: false
+      }
+      return
+    }
+
+    var pokemons = pogobuf.Utils.splitInventory(inventory)['pokemon']
+
+    event.returnValue = {
+      success: true,
+      pokemon: pokemons
+    }
+  })
+})
+// END OF POKEMON
