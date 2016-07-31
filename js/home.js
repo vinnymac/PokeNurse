c4a4213154c1d00a051f68d9aa195dd637f76d5d
jheden <user@admins-Air-2.fritz.box>
2016-07-31 16:57:36 +0200

    Table headers clickable for sorting, full info displayed in IV column, randomized delay between calls

--- js/home.js
+++ js/home.js
@@ -1,18 +1,21 @@
-const ipc = require('electron').ipcRenderer
-const pogoUtils = require('pogobuf').Utils
-const POGOProtos = require('node-pogo-protos')
+const ipc         = require('electron').ipcRenderer
+const pogoUtils   = require('pogobuf').Utils
+const POGOProtos  = require('node-pogo-protos')
 
-const header = document.getElementById('profile-header')
-const usernameH = document.getElementById('username-h')
-const statusH = document.getElementById('status-h')
-const refreshBtn = document.getElementById('refresh-btn')
+const header      = document.getElementById('profile-header')
+const usernameH   = document.getElementById('username-h')
+const statusH     = document.getElementById('status-h')
+const refreshBtn  = document.getElementById('refresh-btn')
 const transferBtn = document.getElementById('transfer-btn')
-const evolveBtn = document.getElementById('evolve-btn')
+const evolveBtn   = document.getElementById('evolve-btn')
 const pokemonList = document.getElementById('pokemon-list')
+const sortLinks   = document.querySelectorAll('td[data-sort]')
 
-var running = false
+var currSorting   = 'pokemon_id'
+var pokemons      = []
+var running       = false
 
-var playerInfo = ipc.sendSync('get-player-info')
+var playerInfo    = ipc.sendSync('get-player-info')
 if (playerInfo.success) {
   switch (playerInfo.player_data['team']) {
     case 1:
@@ -33,9 +36,7 @@ if (playerInfo.success) {
   ipc.send('error-message', 'Failed in retrieving player info.  Please restart.')
 }
 
-refreshBtn.addEventListener('click', () => {
-  refreshPokemonList()
-})
+refreshBtn.addEventListener('click', refreshPokemonList)
 
 transferBtn.addEventListener('click', () => {
   if (runningCheck()) return
@@ -45,9 +46,9 @@ transferBtn.addEventListener('click', () => {
   if (ipc.sendSync('confirmation-dialog', 'transfer').success) {
     running = true
     selectedPokemon.forEach((pokemon, index) => {
-      ipc.send('transfer-pokemon', pokemon.value, index * 2000)
+      ipc.send('transfer-pokemon', pokemon.value, index * randomDelay(2, 3))
     })
-    countDown('Transfer', selectedPokemon.length * 2)
+    countDown('Transfer', selectedPokemon.length * 2.5)
   }
 })
 
@@ -59,36 +60,36 @@ evolveBtn.addEventListener('click', () => {
   if (ipc.sendSync('confirmation-dialog', 'evolve').success) {
     running = true
     selectedPokemon.forEach((pokemon, index) => {
-      ipc.send('evolve-pokemon', pokemon.value, index * 15000)
+      ipc.send('evolve-pokemon', pokemon.value, index * randomDelay(25, 30))
     })
-    countDown('Evolve', selectedPokemon.length * 15)
+    countDown('Evolve', selectedPokemon.length * 27.5)
   }
 })
 
+for(var i=0; i<sortLinks.length; i++)
+  sortLinks[i].addEventListener('click', function(e) {
+    sortPokemonList(this.dataset.sort);
+  });
+
 function refreshPokemonList () {
-  var pokemons = ipc.sendSync('get-players-pokemons')
-  if (pokemons.success) {
-    pokemons.pokemon.sort((a, b) => {
-      if (a['pokemon_id'] < b['pokemon_id']) return -1
-      if (a['pokemon_id'] > b['pokemon_id']) return 1
-      if (a['cp'] > b['cp']) return -1
-      if (a['cp'] < b['cp']) return 1
-      return 0
-    })
+  pokemons = ipc.sendSync('get-players-pokemons')
+  if (pokemons.success)
+    sortPokemonList(currSorting, true);
+}
 
-    pokemonList.innerHTML = ''
+function sortPokemonList (sorting, refresh) {
+  currSorting = (!refresh && sorting == currSorting ? '-' : '') + sorting
+  pokemons.pokemon.sort(sortBy(currSorting))
 
-    pokemons.pokemon.forEach(poke => {
-      var pokemonId = poke['pokemon_id']
+  pokemonList.innerHTML = ''
 
-      var pokemonName = pogoUtils.getEnumKeyByValue(POGOProtos.Enums.PokemonId, pokemonId)
+  pokemons.pokemon.forEach(poke => {
+    var checkBox  = '<input type="checkbox" value="' + poke['id'].toString() + '"'
 
-      var checkBox = '<input type="checkbox" value="' + poke['id'].toString() + '"'
-      if (poke['deployed']) checkBox += ' disabled'
+    if (poke['deployed']) checkBox += ' disabled'
 
-      pokemonList.innerHTML += '<tr><td>' + checkBox + '></td><td>' + pokemonId + '</td><td>' + pokemonName + '</td><td>' + poke['cp'] + '</td><td>' + poke['iv'] + '%</td></tr>'
-    })
-  }
+    pokemonList.innerHTML += '<tr><td>' + checkBox + '></td><td>' + poke['pokemon_id'] + '</td><td>' + poke['name'] + '</td><td>' + poke['cp'] + '</td><td>' + poke['iv'] + '% (' + poke['attack'] + '/' + poke['defense'] + '/' + poke['stamina'] + ')</td></tr>'
+  })
 }
 
 function runningCheck () {
@@ -103,11 +104,30 @@ function countDown (method, index) {
   var interval = setInterval(() => {
     statusH.innerHTML = method + ' / ' + index + ' second(s) left'
     index--
-    if (index === 0) {
+    if (index <= 0) {
       clearInterval(interval)
       running = false
       statusH.innerHTML = 'Idle'
       ipc.send('error-message', 'Complete!')
     }
   }, 1000)
+}
+
+function randomDelay(min, max) {
+  return Math.round((min + Math.random() * (max - min)) * 1000)
+}
+
+function sortBy(prop) {
+  var order = prop.substr(0, 1) == '-' ? -1 : 1;
+
+  if (prop.substr(0, 1) == '-')
+    prop = prop.substr(1);
+
+  return function (a, b) {
+    return  (a[prop] < b[prop]) ? -1 * order :
+            (a[prop] > b[prop]) ? 1 * order :
+            (a['cp'] < b['cp']) ? 1 :
+            (a['cp'] > b['cp']) ? -1 :
+            0;
+  }
 }
