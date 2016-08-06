d7da2e1c4441089832dc8296e688951ae99c3e66
Vincent Taverna <vinny@getlua.com>
2016-08-06 16:06:36 -0400

    modal detail as react component

---
+++ js/screens/Detail/index.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import ModalBody from './components/ModalBody'
+
+import baseStats from '../../../baseStats'
+import cpStats from '../../../cpStats'
+
+const ModalDialog = React.createClass({
+  componentDidMount () {
+    this.props.detailModal.modal('show')
+  },
+
+  render () {
+    return (
+      <div className='modal-dialog' role='document'>
+        <div className='modal-content'>
+          <div className='modal-header'>
+            <button type='button' className='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+            <h4 className='modal-title' id='detailModalLabel'>{this.props.name}</h4>
+          </div>
+          <div className='modal-body'>
+            <ModalBody {...this.props} />
+          </div>
+          <div className='modal-footer'>
+            <button type='button' className='btn btn-default' data-dismiss='modal'>Close</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
+
+export default ($detailModal, {pokemon, species}) => {
+  // Calculate CP Progress dot position
+  let minDeg = 0
+  let maxDeg = 180
+  let degree = Math.max(Math.min((pokemon.cp / pokemon.max_cp) * maxDeg, maxDeg), minDeg)
+
+  let transform = `rotate(${degree}deg) translate(-192px)`
+
+  let stats = baseStats[pokemon['pokemon_id']]
+
+  let baseAttack = stats.BaseAttack
+  let baseDefense = stats.BaseDefense
+
+  // TODO Need additional information to calculate these
+  let hp = `${pokemon.current_stamina} / ${pokemon.stamina_max}`
+  let attack = `${baseAttack + pokemon.attack}`
+  let defense = `${baseDefense + pokemon.defense}`
+  let type = stats.types.join(' / ')
+
+  let cppu = cpStats.cpPerUpgrade[pokemon['pokemon_id'] - 1]
+  let cpPerUpgrade = cppu ? `+${cppu} CP (+/-)` : 'Unknown'
+
+  let height = `${pokemon.height.toFixed(2)}`
+  let weight = `${pokemon.weight.toFixed(2)}`
+
+  let candies = species.candy
+  let name = species.name
+  let nickname = pokemon.nickname
+
+  let spriteImageName = name.toLowerCase()
+  if (spriteImageName.indexOf('nidoran') > -1) {
+    let spriteParts = spriteImageName.split(' ')
+    spriteImageName = `${spriteParts[0]}-${spriteParts[1][0]}`
+  }
+
+  let modalDialog = <ModalDialog
+    name={name}
+    nickname={nickname}
+    transform={transform}
+    attack={attack}
+    defense={defense}
+    hp={hp}
+    candies={candies}
+    cpPerUpgrade={cpPerUpgrade}
+    spriteImageName={spriteImageName}
+    type={type}
+    weight={weight}
+    height={height}
+    detailModal={$detailModal}
+  />
+
+  $detailModal.on('hidden.bs.modal', () => {
+    ReactDOM.unmountComponentAtNode($detailModal.get(0))
+  })
+
+  ReactDOM.render(modalDialog, $detailModal.get(0))
+}
