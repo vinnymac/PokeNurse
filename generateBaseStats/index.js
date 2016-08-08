5476b5de04258ebf95c577b8e19d43c248b41647
Vincent Taverna <vinny@getlua.com>
2016-08-08 14:52:15 -0400

    add script to generate base stats, merged cp stats into base stats

--- generateBaseStats/index.js
+++ generateBaseStats/index.js
@@ -1,26 +1,30 @@
 var fs = require('fs')
 
 var gameMaster = require('./GAME_MASTER_v0_1.json')
+var cpStats = require('./cpStats.json')
+var evolveCost = require('./evolveCost.json')
+var familiesById = require('./familiesById.json')
 
 function writeBaseStats () {
   var baseStats = {}
 
   gameMaster.Items.forEach(item => {
     if (item.TemplateId && item.Pokemon && item.Pokemon.Stats) {
-      let pokemonId = String(parseInt(item.Pokemon.UniqueId.match(/\d+/g)[0]))
+      let pokemonId = parseInt(item.Pokemon.UniqueId.match(/\d+/g)[0])
       let types = []
       if (item.Pokemon.Type1) types.push(item.Pokemon.Type1.split('POKEMON_TYPE_')[1].toLowerCase())
       if (item.Pokemon.Type2) types.push(item.Pokemon.Type2.split('POKEMON_TYPE_')[1].toLowerCase())
 
       baseStats[pokemonId] = {
-        types: types
+        types: types,
+        cpPerUpgrade: cpStats.cpPerUpgrade[pokemonId - 1]
       }
 
-      Object.assign(baseStats[pokemonId], item.Pokemon.Stats)
+      Object.assign(baseStats[String(pokemonId)], item.Pokemon.Stats)
     }
   })
 
-  fs.writeFileSync('./baseStats.json', JSON.stringify(baseStats, null, 2))
+  fs.writeFileSync('./generateBaseStats/baseStats.json', JSON.stringify(baseStats, null, 2))
 }
 
 writeBaseStats()
