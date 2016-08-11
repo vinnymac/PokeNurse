533264a0e767034e49390eed8ae3dc364d4bd53b
Mack Hankins <mack.hankins@gmail.com>
2016-08-11 09:26:56 -0500

    width and stuff

--- js/screens/Table/components/Pokemon.jsx
+++ js/screens/Table/components/Pokemon.jsx
@@ -2,37 +2,56 @@ import React from 'react'
 import renderModal from '../../Detail'
 
 const Pokemon = React.createClass({
-    render () {
-      let {species} = this.props
+  render () {
+    let {species} = this.props
+
+    return (
+      <tr className='child' key={'sub' + species.pokemon_id}>
+        <td colSpan='7'>
+          <table className='table table-condensed table-hover'>
+            <thead>
+              <tr>
+                <th width="5%"><input type="checkbox" /></th>
+                <th width="5%">
+                  <span className='glyphicon glyphicon-star favorite-yellow'></span>
+                </th>
+                <th width="15%">
+                  Name
+                </th>
+                <th>
+                  Nickname
+                </th>
+                <th>
+                  CP
+                </th>
+                <th>
+                  IV
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.getPokemonComponents(species)}
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    )
+  },
+
+  getPokemonComponents (species) {
+    return species.pokemon.map((pokemon) => {
+      let favorite = pokemon.favorite ? 'glyphicon glyphicon-star favorite-yellow' : 'glyphicon glyphicon-star-empty'
+      let favoriteBool = pokemon.favorite ? 'true' : 'false'
+      let pokeiv = pokemon['iv'] + '% (' + pokemon['attack'] + '/' + pokemon['defense'] + '/' + pokemon['stamina'] + ')'
 
       return (
-        <tr className='child' key={'sub' + species.pokemon_id}>
-          <td colSpan='7'>
-            <table className='table table-condensed table-hover'>
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>
-                    <span className='glyphicon glyphicon-star favorite-yellow'></span>
-                  </th>
-                  <th>
-                    Name
-                  </th>
-                  <th>
-                    Nickname
-                  </th>
-                  <th>
-                    CP
-                  </th>
-                  <th>
-                    IV
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.getPokemonComponents(species)}
-              </tbody>
-            </table>
+        <tr key={pokemon.id}>
+          <td>
+            <input
+              type='checkbox'
+              value={String(pokemon.id)}
+              disabled={pokemon.deployed || pokemon.favorite}
+            />
           </td>
         </tr>
       )
