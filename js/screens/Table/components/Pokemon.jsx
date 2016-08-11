34dc72b1aef43440574ba74ac9856b9ca1198d6d
Vincent Taverna <vinny@getlua.com>
2016-08-11 14:22:36 -0400

    add powerup ui and handler

--- js/screens/Table/components/Pokemon.jsx
+++ js/screens/Table/components/Pokemon.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import renderModal from '../../Detail'
 
 const Pokemon = React.createClass({
+  componentDidMount () {
+    $(this.refs.tooltip).tooltip()
+  },
+
+  componentDidUpdate () {
+    $(this.refs.tooltip).tooltip()
+  },
+
   render () {
     let {species} = this.props
 
@@ -11,11 +19,14 @@ const Pokemon = React.createClass({
           <table className='table table-condensed table-hover'>
             <thead>
               <tr>
-                <th width="5%"><input type="checkbox" /></th>
-                <th width="5%">
+                <th width='5%'><input type='checkbox' /></th>
+                <th width='5%'>
                   <span className='glyphicon glyphicon-star favorite-yellow'></span>
                 </th>
-                <th width="15%">
+                <th>
+                  P↑
+                </th>
+                <th width='15%'>
                   Name
                 </th>
                 <th>
@@ -43,6 +54,40 @@ const Pokemon = React.createClass({
       let favorite = pokemon.favorite ? 'glyphicon glyphicon-star favorite-yellow' : 'glyphicon glyphicon-star-empty'
       let favoriteBool = pokemon.favorite ? 'true' : 'false'
       let pokeiv = pokemon['iv'] + '% (' + pokemon['attack'] + '/' + pokemon['defense'] + '/' + pokemon['stamina'] + ')'
+      let powerupComponent
+
+      if (pokemon.cp === pokemon.max_cp) {
+        let tip = `Max CP ${pokemon.max_cp}`
+        powerupComponent = <span
+          data-toggle='tooltip'
+          data-placement='right'
+          data-html='true'
+          title={tip}
+          ref='tooltip'
+        >
+          P↑
+        </span>
+      } else {
+        let tip = `
+        Stardust Cost = ${pokemon.stardust_cost} <br>
+        Candy Cost = ${pokemon.candy_cost} <br>
+        CP After ≅ ${Math.round(pokemon.next_cp) + pokemon.cp} <br>
+        Max Stardust = ${pokemon.stardust_max_cost} <br>
+        Max Candy = ${pokemon.candy_max_cost}
+        `
+        powerupComponent = <a
+          id='powerUp'
+          data-pokemon-id={pokemon.id}
+          data-nickname={pokemon.nickname}
+          data-toggle='tooltip'
+          data-placement='right'
+          data-html='true'
+          title={tip}
+          ref='tooltip'
+        >
+          P↑
+        </a>
+      }
 
       return (
         <tr key={pokemon.id}>
@@ -65,50 +110,57 @@ const Pokemon = React.createClass({
 
             return (
                 <tr key={pokemon.id}>
-                    <td>
-                        <input
-                            type='checkbox'
-                            value={String(pokemon.id)}
-                            disabled={pokemon.deployed}
-                        />
-                    </td>
-                    <td>
-            <span
-                className={`favorite ${favorite}`}
-                id='favoriteBtn'
-                data-pokemon-id={pokemon.id}
-                data-pokemon-favorited={favoriteBool}
-                onClick={this.handleClickFavorite.bind(this, pokemon)}
-            />
-                    </td>
-                    <td>
-                        {pokemon.name}
-                    </td>
-                    <td onClick={this.handleClickNickname.bind(this, pokemon, species)}>
-                        <a
-                            className='nickname'
-                            data-pokemon-id={pokemon.id}
-                        >
-                            {pokemon.nickname}
-                        </a>
-                    </td>
-                    <td>
-                        {pokemon.cp}
-                    </td>
-                    <td>
-                        {pokeiv}
-                    </td>
+                  <td>
+                    <input
+                        type='checkbox'
+                        value={String(pokemon.id)}
+                        disabled={pokemon.deployed}
+                    />
+                  </td>
+                  <td>
+                    <span
+                        className={`favorite ${favorite}`}
+                        id='favoriteBtn'
+                        data-pokemon-id={pokemon.id}
+                        data-pokemon-favorited={favoriteBool}
+                        onClick={this.handleClickFavorite.bind(this, pokemon)}
+                    />
+                  </td>
+                  <td onClick={this.handleClickPowerup.bind(this, pokemon)}>
+                    {powerupComponent}
+                  </td>
+                  <td>
+                    {pokemon.name}
+                  </td>
+                  <td onClick={this.handleClickNickname.bind(this, pokemon, species)}>
+                    <a
+                      className='nickname'
+                      data-pokemon-id={pokemon.id}
+                    >
+                      {pokemon.nickname}
+                    </a>
+                  </td>
+                  <td>
+                    {pokemon.cp}
+                  </td>
+                  <td>
+                    {pokeiv}
+                  </td>
                 </tr>)
-        })
-    },
+    })
+  },
 
-    handleClickFavorite (pokemon, e) {
-        console.log(pokemon)
-    },
+  handleClickPowerup (pokemon, e) {
+    console.log(pokemon)
+  },
 
-    handleClickNickname (pokemon, species, e) {
-        renderModal($(document.getElementById('detailModal')), pokemon, species)
-    }
+  handleClickFavorite (pokemon, e) {
+    console.log(pokemon)
+  },
+
+  handleClickNickname (pokemon, species, e) {
+      renderModal($(document.getElementById('detailModal')), pokemon, species)
+  }
 
 })
 
