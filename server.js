e55ea25fffc4671d20d0c7db8d61173975ddb682
Vincent Taverna <vinny@getlua.com>
2016-08-17 19:58:11 -0400

    make dashboard optional while it works out bugs

--- server.js
+++ server.js
@@ -9,25 +9,32 @@ import DashboardPlugin from 'webpack-dashboard/plugin'
 
 import config from './webpack.config.development'
 
+const argv = require('minimist')(process.argv.slice(2))
+
+const PORT = 3009
 const app = express()
 const compiler = webpack(config)
-const dashboard = new Dashboard()
-compiler.apply(new DashboardPlugin(dashboard.setData))
-const PORT = 3009
 
-const wdm = webpackDevMiddleware(compiler, {
-  quiet: true,
+if (argv.dashboard) {
+  const dashboard = new Dashboard()
+  compiler.apply(new DashboardPlugin(dashboard.setData))
+}
+
+const webpackDevMiddlwareConfig = {
+  quiet: argv.dashboard,
   publicPath: config.output.publicPath,
   stats: {
     colors: true
   }
-})
+}
+
+const wdm = webpackDevMiddleware(compiler, webpackDevMiddlwareConfig)
 
 app.use(wdm)
 
-app.use(webpackHotMiddleware(compiler, {
-  log: () => {}
-}))
+const webpackHotMiddlewareConfig = argv.dashboard ? { log: () => {} } : {}
+
+app.use(webpackHotMiddleware(compiler, webpackHotMiddlewareConfig))
 
 const server = app.listen(PORT, 'localhost', err => {
   if (err) {
