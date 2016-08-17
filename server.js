c76e8515bee653256796f5a5ef30aa417b799200
Vincent Taverna <vinny@getlua.com>
2016-08-17 10:49:41 -0400

    add webpack-dashboard

--- server.js
+++ server.js
@@ -4,14 +4,19 @@ import express from 'express'
 import webpack from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
+import Dashboard from 'webpack-dashboard'
+import DashboardPlugin from 'webpack-dashboard/plugin'
 
 import config from './webpack.config.development'
 
 const app = express()
 const compiler = webpack(config)
+const dashboard = new Dashboard()
+compiler.apply(new DashboardPlugin(dashboard.setData))
 const PORT = 3009
 
 const wdm = webpackDevMiddleware(compiler, {
+  quiet: true,
   publicPath: config.output.publicPath,
   stats: {
     colors: true
@@ -20,7 +25,9 @@ const wdm = webpackDevMiddleware(compiler, {
 
 app.use(wdm)
 
-app.use(webpackHotMiddleware(compiler))
+app.use(webpackHotMiddleware(compiler, {
+  log: () => {}
+}))
 
 const server = app.listen(PORT, 'localhost', err => {
   if (err) {
