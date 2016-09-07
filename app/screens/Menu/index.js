bd28fcdcd711174cad889274932c6bf08a6b86ce
Vincent Taverna <vinny@getlua.com>
2016-09-06 21:12:54 -0400

    hide unused menu items

--- app/screens/Menu/index.js
+++ app/screens/Menu/index.js
@@ -24,10 +24,12 @@ const MainMenu = React.createClass({
         <div>
           <ul className="nav navbar-nav">
             <li><a onClick={this.handleClickSettings}>Settings <i className="glyphicon glyphicon-cog" /></a></li>
-            <li><a>Trainer Info <i className="glyphicon glyphicon-user" /></a></li>
-            <li><a>Applied Items <i className="glyphicon glyphicon-chevron-down" /></a></li>
-            <li><a>Inventory <i className="glyphicon glyphicon-chevron-down" /></a></li>
-            <li><a>Eggs <i className="glyphicon glyphicon-chevron-down" /></a></li>
+            {
+              // <li><a>Trainer Info <i className="glyphicon glyphicon-user" /></a></li>
+              // <li><a>Applied Items <i className="glyphicon glyphicon-chevron-down" /></a></li>
+              // <li><a>Inventory <i className="glyphicon glyphicon-chevron-down" /></a></li>
+              // <li><a>Eggs <i className="glyphicon glyphicon-chevron-down" /></a></li>
+            }
             <li><a onClick={this.handleSignOut}>Sign Out <i className="glyphicon glyphicon-off" /></a></li>
           </ul>
         </div>
