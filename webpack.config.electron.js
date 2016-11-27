60081eb5e7aa996567203c2f347ed875fefbf686
Vincent Taverna <vinnymac@gmail.com>
2016-11-27 16:46:16 -0500

    more electron and production changes to webpack config

--- webpack.config.electron.js
+++ webpack.config.electron.js
@@ -32,17 +32,23 @@ export default validate(merge(baseConfig, {
     })
   ],
 
+  /**
+     * Set target to Electron specific node.js env.
+     * https://github.com/chentsulin/webpack-target-electron-renderer#how-this-module-works
+     */
   target: 'electron-main',
 
+  /**
+     * Disables webpack processing of __dirname and __filename.
+     * If you run the bundle in node.js it falls back to these values of node.js.
+     * https://github.com/webpack/webpack/issues/2010
+     */
   node: {
     __dirname: false,
     __filename: false
   },
 
   externals: [
-    // 'source-map-support',
     'electron-devtools-installer',
-  ],
-
-  noParse: /json-schema\/lib\/validate\.js/
+  ]
 }))
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import webpack from 'webpack'
 import validate from 'webpack-validator'
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
@@ -14,6 +15,7 @@ const config = validate(merge(baseConfig, {
   ],
 
   output: {
+    path: path.join(__dirname, 'app/dist'),
     publicPath: '../dist/'
   },
 
